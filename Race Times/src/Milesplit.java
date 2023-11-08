dec0609b86d9f0902dca2d6b5d0af6e4a51a3d25
genevieve.hockin <genevieve.hockin@MU-C1-04.mullen.pvt.k12.co.us>
2023-11-08 14:51:47 -0700

    Contains the new user input and output

--- Race Times/src/Milesplit.java
+++ Race Times/src/Milesplit.java
@@ -1,32 +1,41 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-class Milesplit
-	{
-		
-		public static void main(String[] args)
-			{
-				    Times.fillArray();
-				    Scanner userInput = new Scanner(System.in);
-				    System.out.println("Input a time and based on that I will tell --- don't use : when writing your time; for  a time of 14:50 just write 1450 ");  ///tell the person what the code will do- input their time and pull
-				 //   int average = userInput.nextInt();
-				    Times.fillArray();
-				    
-				    }
-		public static void personalTime(String[] args)
-		{
-			  Scanner scanner = new Scanner(System.in);
+public class Milesplit {
+    public static void main(String[] args) {
+        Times.fillArray(); // Fill the initial list of runners
 
-		        System.out.print("Enter something: ");
-		        String userInput = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-		        System.out.println("You entered: " + userInput);
-	
-	          // if( <=1448 || average >=1558){///option 3
-	         //    System.out.println("You are between " + average.getName());
-	             
-	           }
-		} 
+        System.out.print("Enter your name:");
+        String newName = scanner.nextLine();
+
+        System.out.print("Enter the time (in the form of 1448 if running a 14:48 ) for the new runner: ");
+        int newTime = scanner.nextInt();
+
+        Runner newRunner = new Runner(newName, newTime);
+
+        insertRunnerInOrder(newRunner, Times.time);
+
+        // Print the updated list of runners
+        System.out.println("Updated List of Runners:");
+        for (int i = 0; i < Times.time.size(); i++) {
+            Runner runner = Times.time.get(i);
+            System.out.println((i + 1) + ")" + runner.getName() + " ~ " + runner.getTime());
+        }
+    }
+
+    public static void insertRunnerInOrder(Runner newRunner, ArrayList<Runner> runners) {
+        int newIndex = 0;
+
+        while (newIndex < runners.size() && newRunner.getTime() > runners.get(newIndex).getTime()) {
+            newIndex++;
+        }
+
+        runners.add(newIndex, newRunner);
+    }
+}
+
 
 				  
 				      
--- Race Times/src/Times.java
+++ Race Times/src/Times.java
@@ -5,15 +5,15 @@ public class Times
 				 static ArrayList<Runner> time = new ArrayList<Runner>();
 				    public static void fillArray()
 				        {
-				        	time.add(new Runner(" Porter ", 1438 ));
+				        	time.add(new Runner("Porter ", 1438 ));
 				        	time.add(new Runner("Will ", 1446 ));
-				        	time.add(new Runner(" Benjamin",1448 ));
+				        	time.add(new Runner("Benjamin",1448 ));
 				        	time.add(new Runner("Daniel ",1449 ));
 				        	time.add(new Runner ("Jacob ",1449 ));
 				        	time.add(new Runner( "Matthew ", 1454));
 				        	time.add(new Runner ("Owen C", 1454 ));
-				        	time.add(new Runner (" Owen W", 1501 ));
-				        	time.add(new Runner (" Kaeden", 1502 ));
+				        	time.add(new Runner ("Owen W", 1501 ));
+				        	time.add(new Runner ("Kaeden", 1502 ));
 				        	time.add(new Runner ("Brennan ", 1508 ));
 				        	time.add(new Runner("Tanner ", 1508 ));
 				        	time.add(new Runner ("Oliver ", 1510 ));
